a50604d3be09ecc625a482d0a40297769f360951
Elizaveta Sharkova <sharkliza@gmail.com>
2019-09-08 12:45:47 +0300

    send request to Bing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.Net;
+using System.IO;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -10,8 +13,16 @@ namespace MySearch.Controllers
 {
     public class HomeController : Controller
     {
+        //todo: delete it, add in db
+        private const string bingAccessKey = "";
+        private const string uriBase = "https://api.cognitive.microsoft.com/bing/v7.0/search";
+        //private const string searchTerm = "Microsoft Cognitive Services";
+
+        //public string results;
+
         public IActionResult Index()
         {
+             // = results;
             return View();
         }
 
@@ -25,5 +36,131 @@ namespace MySearch.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult Index(string searchTerm)
+        {
+            string results;
+            if (bingAccessKey.Length == 32)
+            {
+                SearchResult result = BingWebSearch(searchTerm);
+
+                results = JsonPrettyPrint(result.jsonResult);
+            }
+            else
+            {
+                results = "Invalid Bing Search API subscription key!";
+            }
+
+            ViewBag.results = results;
+            return View();
+        }
+
+        /// <summary>
+        /// Makes a request to the Bing Web Search API and returns data as a SearchResult.
+        /// </summary>
+        static SearchResult BingWebSearch(string searchQuery)
+        {
+            // Construct the search request URI.
+            var uriQuery = uriBase + "?q=" + Uri.EscapeDataString(searchQuery);
+
+            // Perform request and get a response.
+            WebRequest request = HttpWebRequest.Create(uriQuery);
+            request.Headers["Ocp-Apim-Subscription-Key"] = bingAccessKey;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponseAsync().Result;
+            string json = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            // Create a result object.
+            var searchResult = new SearchResult()
+            {
+                jsonResult = json,
+                relevantHeaders = new Dictionary<String, String>()
+            };
+
+            // Extract Bing HTTP headers.
+            foreach (String header in response.Headers)
+            {
+                if (header.StartsWith("BingAPIs-") || header.StartsWith("X-MSEdge-"))
+                    searchResult.relevantHeaders[header] = response.Headers[header];
+            }
+            return searchResult;
+        }
+
+        /// <summary>
+        /// Formats the JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            char last = ' ';
+            int offset = 0;
+            int indentLength = 2;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\\':
+                        if (quote && last != '\\') ignore = true;
+                        break;
+                }
+
+                if (quote)
+                {
+                    sb.Append(ch);
+                    if (last == '\\' && ignore) ignore = false;
+                }
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case ']':
+                        case '}':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (quote || ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+                last = ch;
+            }
+            return sb.ToString().Trim();
+        }
+    }
+
+    public struct SearchResult
+    {
+        public String jsonResult;
+        public Dictionary<String, String> relevantHeaders;
     }
 }
